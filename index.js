7119bac7982de03c7cb36df226b107ecbd32db84
Brenda <brendamelop8@gmail.com>
2024-11-08 03:12:20 -0300

    Continuando...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.resolve() + '/Imagens'));
 app.use(express.urlencoded({extend: true}));
 
 const porta = 3000;
@@ -103,54 +105,78 @@ function menu(req, resp){
     resp.send(`
             <html>
                 <head>
-                    <title>Criar Conta no CinePrime</title>
+                    <title>CinePrime</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap" rel="stylesheet">
+
+                    <style>
+                        img {
+                                max-width: 100%;
+                                height: auto;
+                            }
+
+                        .navbar {
+                            background-color: #BFBFBF;
+                            box-shadow: none; 
+                            border-bottom: none !important;
+                        }
+
+                        .navbar-brand, .nav-link {
+                            font-family: 'Cinzel', serif;
+                            font-weight: bold;
+                            color: #0A8081 !important;
+                        }
+                    </style>
                 </head>
                 <body>
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <nav class="navbar navbar-expand-lg">
                         <div class="container-fluid">
                             <a class="navbar-brand" href="#">
-                                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                                <img src="Imagens/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
                                 CINEPRIME
                             </a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                                     <li class="nav-item">
                                         <a class="nav-link active" aria-current="page" href="/cadastrar">Criar Conta</a>
                                     </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="#">Action</a></li>
-                                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                                            <li><hr class="dropdown-divider"></li>
-                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                                    </li>
                                 </ul>
-                                <form class="d-flex" role="search">
-                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                    <button class="btn btn-outline-success" type="submit">Search</button>
-                                </form>
                             </div>
                         </div>
                     </nav>
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="Imagens/FilmeAção.jpg" class="d-block w-100" alt="Filme de Ação">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="Imagens/FilmeRomance.jpeg" class="d-block w-100" alt="Filme de Romance">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="Imagens/FilmeTerror2.jpeg" class="d-block w-100" alt="Filme de Terror">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
                 </body>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </html>
         `);
-
 }
 
 function cadastrarUsuario(req, resp){
